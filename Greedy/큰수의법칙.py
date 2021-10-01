0102f917f13a8950d342814763cb8cee8baf4f07
Youngjun Cho <55816062+youngjuncho729@users.noreply.github.com>
2021-10-01 13:24:13 +0000

    Added Shortest_Path Section

---
+++ Greedy/큰수의법칙.py
@@ -0,0 +1,40 @@
+""" 큰수의 법칙   
+n: number of given numbers
+m: number of addition
+k: max number of continuously adding same index 
+
+input example:
+  5 8 3      (n, m, k)
+  2 4 5 4 6  (list of numbers)
+"""
+# first line input
+n, m, k = map(int, input().split())
+# second line input
+data = list(map(int, input().split()))
+
+data.sort()
+max1 = data[n - 1]
+max2 = data[n - 2]
+
+result = 0
+
+""" Easy Way """
+# while True:
+#   for i in range(k):
+#     if m == 0:
+#       break
+#     result += max1
+#     m -= 1
+#   if m == 0:
+#     break
+#   result += max2
+#   m -= 1
+
+""" Faster Way """
+# number of adding max1
+count = (m // (k + 1)) * k + m % (k + 1)
+
+result = count * max1
+result += (m - count) * max2
+
+print("result is " + str(result))
---
+++ Shortest_Path/Dijkastra.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+# number of nodes and edges
+n, m = map(int, input().split())
+# starting node
+start = int(input())
+# create a list of info about adjacent of each node
+graph = [[] for i in range(n + 1)]
+# either each node is visited or unvisited
+visited = [False] * (n + 1)
+# set the default distance to INF
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+  a, b, c = map(int, input().split())
+  graph[a].append((b, c))
+
+# Get the unvisited node with shortest distance
+def get_smallest_node():
+  min_value = INF
+  index = 0
+  for i in range(1, n + 1):
+    if distance[i] < min_value and not visited[i]:
+      min_value = distance[i]
+      index = i
+  return index
+
+def dijkstra(start):
+    # reset the starting node
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # repeat each node except the starting node
+    for i in range(n - 1):
+        # set the shorted distance node to visited
+        now = get_smallest_node()
+        visited[now] = True
+        # Check all adjacent node
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # if the path through other node is shorter
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
