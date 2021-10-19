ba171b96bac6e6d3321fe3758f83f1535c833a64
Youngjun Cho <55816062+youngjuncho729@users.noreply.github.com>
2021-10-19 14:12:14 +0000

    Solved Snake problem

--- Implementation/Snake.py
+++ Implementation/Snake.py
@@ -8,22 +8,19 @@ turn = []
 for _ in range(L):
   x, c = input().split()
   turn.append([int(x), c])
-
-
 board = [[0] * N for _ in range(N)]
 for apple in apples:
   board[apple[0] - 1][apple[1] - 1] = 1
-print(board)
 
-def move(diretion, head):
+def move(direction, head):
   if direction == "right":
-    head[0] += 1
-  elif direction == "left":
-    head[0] -= 1
-  elif direction == "up":
-    head[1] -= 1
-  else:
     head[1] += 1
+  elif direction == "left":
+    head[1] -= 1
+  elif direction == "up":
+    head[0] -= 1
+  else:
+    head[0] += 1
 
 def turnHead(direction, turn):
   if direction == "right" and turn == "L" or direction == "left" and turn == "D":
@@ -36,7 +33,7 @@ def turnHead(direction, turn):
     return "right"
 
 def gameOver(head, body):
-  if N < head[0] or N < head[1] or head[0] < 0 or head[1] < 0:
+  if N <= head[0] or N <= head[1] or head[0] < 0 or head[1] < 0:
     return True
   if head in body:
     return True
@@ -44,29 +41,28 @@ def gameOver(head, body):
 
 head = [0, 0]  # Coordinate of the snake head
 body = deque() # list of snake body coordinate
-length = 1
 direction = "right" # starting diretion
 sec = 0   # how may seconds after game start
 over = False
 next_turn = 0   # index of the next turn
 
-
 while over == False:
   sec += 1
+  prev_x, prev_y = head[0], head[1]
+    # Eat the apple
   move(direction, head)
   # Check if snake hits the wall or its body
   if gameOver(head, body):
     over = True
-    sec -= 1
   # Make the turn if it needs
   if next_turn < L and turn[next_turn][0] == sec:
     direction = turnHead(direction, turn[next_turn][1])
     next_turn += 1
-  # Eat the apple
-  #if board[head[0]][head[1]] == 1:
-
-  
-
-  
-
+  # Add the body if snake eats apple
+  if 0 <= head[0] < N and 0 <= head[1] < N:
+    body.appendleft([prev_x, prev_y])
+    if board[head[0]][head[1]] == 1:
+      board[head[0]][head[1]] = 0
+    else:
+      body.pop()
 print(sec)
