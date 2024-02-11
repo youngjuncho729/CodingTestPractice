d897469ef67b947a3a90659c57ac86dcb31695c8
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2024-02-11 15:59:40 -0500

    Update BoardingGate.py

--- GraphTheory/BoardingGate.py
+++ GraphTheory/BoardingGate.py
@@ -24,7 +24,7 @@ def find_parent(parent, x):
     parent[x] = find_parent(parent, parent[x])
   return parent[x]
 
-def union_paren(parent, x, y):
+def union_parent(parent, x, y):
   a = find_parent(parent, x)
   b = find_parent(parent, y)
   if a < b:
@@ -42,7 +42,7 @@ def solution():
     data = find_parent(parent, int(input()))
     if data == 0:
       break
-    union_paren(parent, data, data - 1)
+    union_parent(parent, data, data - 1)
     result += 1
   print(result)
 
